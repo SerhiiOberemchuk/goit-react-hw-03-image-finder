b0017a227d4f11cc6b2cc2bf22f4a2aed4244ecc
SerhiiOberemchuk <gardenitparts@gmail.com>
2023-12-15 10:37:30 +0100

    2

--- src/Services/Api.js
+++ src/Services/Api.js
@@ -1,15 +1,15 @@
-// import axios from 'axios';
+import axios from 'axios';
 
-// const apiKey = '40336421-a348c8518e766dd2004df0c10';
-// let pages = 1;
-// const per_page = 12;
-// const baseURL = 'https://pixabay.com/api/';
+const apiKey = '40336421-a348c8518e766dd2004df0c10';
+let pages = 1;
+const per_page = 12;
+const baseURL = 'https://pixabay.com/api/';
 
-// export const fetchGalleryItems = async searchQuery => {
-//   const response = await axios.get(
-//     `${baseURL}?q=${searchQuery}&key=${apiKey}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${per_page}&page=${pages}`
-//   );
-//   return response.data.hits;
-// };
+export const fetchGalleryItems = async searchQuery => {
+  const response = await axios.get(
+    `${baseURL}?q=${searchQuery}&key=${apiKey}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${per_page}&page=${pages}`
+  );
+  return response.data.hits;
+};
 
-// export default { fetchGalleryItems };
+export default { fetchGalleryItems };
