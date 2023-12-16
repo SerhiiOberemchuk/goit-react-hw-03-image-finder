04360b5512e034867309566740d4331e3b305317
SerhiiOberemchuk <gardenitparts@gmail.com>
2023-12-16 21:19:26 +0100

    16,12,21,19

--- src/Services/Api.js
+++ src/Services/Api.js
@@ -6,9 +6,8 @@ const BASE_URL = 'https://pixabay.com/api';
 // let pages = 1;
 const per_page = 12;
 
-const fetchGalleryItems = async (searchText, pages) => {
+export const fetchGalleryItems = async (searchText, pages) => {
   return await axios.get(
     `${BASE_URL}/?q=${searchText}&key=${API_KEY}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${per_page}&page=${pages}`
   );
 };
-export default fetchGalleryItems;
